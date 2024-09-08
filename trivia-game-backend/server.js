5db2855bbb092622fe0cb85410b39724db0fa189
LiviuCaciulatu <liviucaciulatu@gmail.com>
2024-09-08 14:37:07 +0300

    backend

---
+++ trivia-game-backend/server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+const cors = require('cors');
+
+const app = express();
+const pool = new Pool({
+    user: 'Liviu',
+    host: 'localhost',
+    database: 'trivia_users',
+    password: 'Converse10',
+    port: 5432,
+});
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/signup', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const existingUser = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+        if (existingUser.rows.length > 0) {
+            return res.status(400).json({ message: 'Username already exists' });
+        }
+
+        await pool.query(
+            'INSERT INTO users (username, password) VALUES ($1, $2)',
+            [username, password]
+        );
+
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
+app.post('/signin', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const result = await pool.query('SELECT * FROM users WHERE username = $1 AND password = $2', [username, password]);
+
+        if (result.rows.length === 0) {
+            return res.status(400).json({ message: 'Invalid username or password' });
+        }
+
+        res.status(200).json({ message: 'Login successful' });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error1234' });
+    }
+});
+
+app.listen(5000, () => {
+    console.log('Server running on port 5000');
+});
+
