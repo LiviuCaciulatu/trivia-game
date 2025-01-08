1e7ddfc2b50d1be79ce4923da91dbce2178c7001
LiviuCaciulatu <liviucaciulatu@gmail.com>
2025-01-08 21:09:52 +0200

    start navbar

--- app/components/Navbar/page.tsx
+++ app/components/Navbar/page.tsx
@@ -10,6 +10,7 @@ const Navbar = () =>{
                 <div className={style.logo}>
                     <Image width={70} height={70} src="/svg/Logo-3.svg" alt="about us" className={style.logoImg} />
                     <h1>Trivia</h1>
+                    <h2>home</h2>
                 </div>
             </div>
 
