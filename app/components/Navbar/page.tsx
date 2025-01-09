337e5ea4789c333229599ebd243bce3289ef8aac
LiviuCaciulatu <liviucaciulatu@gmail.com>
2025-01-09 14:50:11 +0200

    centered navbar

--- app/components/Navbar/page.tsx
+++ app/components/Navbar/page.tsx
@@ -1,21 +1,19 @@
 import Image from "next/image";
-import style from "./style.module.scss"
-
+import style from "./style.module.scss";
 import React from "react";
 
-const Navbar = () =>{
-    return (
-        <div className={style.container}>
-            <div className={style.navBar}>
-                <div className={style.logo}>
-                    <Image width={70} height={70} src="/svg/Logo-3.svg" alt="about us" className={style.logoImg} />
-                    <h1>Trivia</h1>
-                    <h2>home</h2>
-                </div>
-            </div>
-
+const Navbar = () => {
+  return (
+    <div className={style.container}>
+      <div className={`${style.navBar}`}>
+        <div className={`${style.logo}`}>
+          <Image width={70} height={70} src="/svg/Logo-3.svg" alt="about us" className={style.logoImg} />
+          <h1 className={`${style.name}`}>Trivia</h1>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
+
