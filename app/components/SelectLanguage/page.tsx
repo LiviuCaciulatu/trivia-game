98dc87145b9705861d8259728e8641514065203f
LiviuCaciulatu <liviucaciulatu@gmail.com>
2025-01-12 16:36:41 +0200

    select language

--- app/components/SelectLanguage/page.tsx
+++ app/components/SelectLanguage/page.tsx
@@ -1,18 +1,37 @@
-import style from "./style.module.scss"
-import React from "react";
+"use client"
+import React from 'react';
+import style from './style.module.scss';
+import { useLanguage } from '../../context/LanguageContext';
 
-const SelectLanguage = () =>{
-    return (
-        <div className={style.container}>
-            <div className={style.selectLanguage}>
-                <div className={style.selector}>    
-                    <button className={`${style.btnEn} btn btn-info`}>English</button>
-                    <button className={`${style.btnRo} btn btn-info`}>Romana</button>
-                </div>
-            </div>
+const SelectLanguage: React.FC = () => {
+  const { setLanguage } = useLanguage();
 
+  const handleLanguageChange = (lang: string) => {
+    setLanguage(lang);
+    console.log(lang)
+  };
+
+  return (
+    <div className={style.container}>
+      <div className={style.selectLanguage}>
+        <div className={style.selector}>
+          <h2 className={style.title}>Select Language</h2>
+          <button
+            onClick={() => handleLanguageChange('en')}
+            className={`${style.btnEn} btn btn-info`}
+          >
+            English
+          </button>
+          <button
+            onClick={() => handleLanguageChange('ro')}
+            className={`${style.btnRo} btn btn-info`}
+          >
+            Romana
+          </button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default SelectLanguage;
